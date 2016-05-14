c868b5321a58820f6c0593639e302e99e3ea1797
Tomo Masakura <tomo.masakura@gmail.com>
2016-05-14 15:06:03 +0900

    投稿できるように

---
+++ bbs/board-manager.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const _ = require('underscore');
+
+class BoardManager {
+  constructor(db) {
+    this.db = db;
+  }
+
+  getAll() {
+    const sql = 'select * from boards';
+    return this.db.each(sql);
+  }
+  getById(id) {
+    const sql = `select * from boards where id = ${id}`;
+    return this.db.each(sql)
+      .then(rows => _.first(rows));
+  }
+  add(post) {
+    const sql = 'insert into boards(title, body, owner)' +
+            ` values('${post.title}', '${post.body}',  '${post.owner}')`;
+    return this.db.run(sql);
+  }
+  save(post) {
+    const sql = 'update boards' +
+            ` set title = '${post.title}', body = '${post.body}', owner = '${post.owner}'` +
+            ` where id = ${post.id}`;
+    return this.db.run(sql);
+  }
+  remove(id) {
+    const sql = `delete from boards where id = ${id}`;
+    return this.db.run(sql);
+  }
+}
+
+module.exports = BoardManager;
