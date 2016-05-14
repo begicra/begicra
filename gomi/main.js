45c00aac32e50187af7c704d8d4efe275a028942
Tomo Masakura <tomo.masakura@gmail.com>
2016-05-14 16:01:25 +0900

    リファクタリング

--- gomi/main.js
+++ gomi/main.js
@@ -9,32 +9,41 @@
 
     const ret = JSON.parse(event.data);
 
-    if (!ret.type) {
-      console.log('データが空です');
-    } else if (ret.type === 'sql/query') {
+    function sqlQuery(ret) {
       console.log(ret.sql);
       const $query = $('<div class="alert alert-success"></div>')
               .text(ret.sql);
-      $('#retSelectBox').empty().append($query);
-    } else if (ret.type === 'sql/result') {
+
+      return $query;
+    }
+
+    function sqlResult(ret) {
       console.log(ret.rows);
-      console.log(ret.rows.length);
 
       if (ret.rows.length <= 0) {
-        $('#retResultBox').empty().append('Empty Result');
-      } else {
-        const $rows = ret.rows.map(row => $('<tr></tr>')
-                                   .append($('<td></td>').text(row.id))
-                                   .append($('<td></td>').text(row.name))
-                                   .append($('<td></td>').text(row.password)));
-
-        const $tr = $('<tr></tr>').append($rows);
-        const $table = $('<table class="table table-bordered table-striped">' +
-                         '<tr><th>id</th><th>name</th><th>password</th></tr></table>');
-        $table.append($tr);
-
-        $('#retResultBox').empty().append($table);
+        $('#retResultBox').empty().append('データなし');
+        return $('データなし');
       }
+
+      const $rows = ret.rows.map(row => $('<tr></tr>')
+                                 .append($('<td></td>').text(row.id))
+                                 .append($('<td></td>').text(row.name))
+                                 .append($('<td></td>').text(row.password)));
+
+      const $tr = $('<tr></tr>').append($rows);
+      const $table = $('<table class="table table-bordered table-striped">' +
+                       '<tr><th>id</th><th>name</th><th>password</th></tr></table>');
+      $table.append($tr);
+
+      return $table;
+    }
+
+    if (!ret.type) {
+      console.log('データが空です');
+    } else if (ret.type === 'sql/query') {
+      $('#retSelectBox').empty().append(sqlQuery(ret));
+    } else if (ret.type === 'sql/result') {
+      $('#retResultBox').empty().append(sqlResult(ret));
     }
   };
 
