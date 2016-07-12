aa57e3cf5c91139a329aa12ba27606d340ae3fb7
Tomo Masakura <tomo.masakura@gmail.com>
2016-07-12 23:30:25 +0900

    ダッシュボードから、新しい環境を起動できるように

--- dashbaord/dashboard.js
+++ dashbaord/dashboard.js
@@ -1,9 +1,20 @@
 'use strict';
 
 const path = require('path');
+const crypto = require('crypto');
 const express = require('express');
 const router = express.Router(); // eslint-disable-line new-cap
 
+function newId() {
+  const value = Math.random().toString();
+  const md5 = crypto.createHash('md5');
+  md5.update(value, 'utf8');
+  return md5.digest('hex');
+}
+
 router.use('/', express.static(path.join(__dirname, 'static')));
+router.post('/new', (req, res) => {
+  res.redirect(path.join('/app', newId(), 'bbs'));
+});
 
 module.exports = router;
