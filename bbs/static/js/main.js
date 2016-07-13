7bc15f694e1fc046541708dee1ac8c32c4a28c69
Tomo Masakura <tomo.masakura@gmail.com>
2016-07-13 23:42:43 +0900

    ヒントを ON/OFF できるようにした。

--- bbs/static/js/main.js
+++ bbs/static/js/main.js
@@ -1,7 +1,46 @@
 (() => {
   'use strict';
 
+  const config = {
+    get showHint() {
+      return localStorage.getItem('begicra:show-hint') !== 'false';
+    },
+    toggleShowHint() {
+      const showHint = !this.showHint;
+      localStorage.setItem('begicra:show-hint', showHint);
+      return showHint;
+    },
+  };
+
+  const hintManager = {
+    hint(show) {
+      if (show) {
+        this.showHint();
+      } else {
+        this.hideHint();
+      }
+    },
+    showHint() {
+      $('.bbs-hint').popover('show');
+    },
+    hideHint() {
+      $('.bbs-hint').popover('hide');
+    },
+  };
+
+  $(() => {
+    $('.bbs-hint').popover({
+      animattion: true,
+      trigger: 'manual',
+      html: true,
+    });
+
+    hintManager.hint(config.showHint);
+  });
+
   $(document).on('click', '.bbs-show-hint', () => {
-    alert('作成中です! ごめんなさい!');
+    const showHint = config.toggleShowHint();
+
+    $('.bbs-hint').popover(showHint ? 'show' : 'hide');
   });
 })();
